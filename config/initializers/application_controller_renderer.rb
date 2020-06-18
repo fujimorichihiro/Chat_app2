368ad11ec59c7886ff9f265569bf609fd6c8c9c0
fujimorichihiro <fujimori.drum@gmail.com>
2020-06-18 16:32:48 +0900

    Create Repository

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '569ce38f47fc232cc582995963e6c2a6877766a89082c64b4d450bc34433ded59febc3a8868cdf34aac255c108a17f7d7c52b545baf87c7d91ec9b21159cd619'
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  
+  devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
+  root 'home#top'
+
+  resources :users, only: [:show, :index, :edit, :update ]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
